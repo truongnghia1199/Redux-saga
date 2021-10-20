07979d4b59413c59039c5a5664e3be30739393b0
truongnghia1199 <truongnghia1199@gmail.com>
2021-10-20 17:00:44 +0700

    ver-1

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AddEditUser from './pages/AddEditUser';
+import UserInfo from './pages/UserInfo';
+import About from './pages/About';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/addUser" component={AddEditUser} />
+          <Route path="/editUser/:id" component={AddEditUser} />
+          <Route path="/userInfo/:id" component={UserInfo} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  )
+}
+export default App
